b9a94853a231c6f4c144ea0aaebb3193af58dbcd
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-07-16 22:58:54 -0700

    pkgdown edits

--- R/data.R
+++ R/data.R
@@ -39,11 +39,12 @@ NULL
 #' head(biol_dat)
 NULL
 
-#' YFT_4area_observations_1-100
+#' 1 AREA SINGLE REPRESENTATIVE DATASET
 #'
-#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#' A single representative datasets aggregated at the 1 area scale. The chosen dataset is simulation #4.
+#' List objects include aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
 #'
-#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' @format A data frame 'YFT_SRD_1A_4.RData' with variables:
 #' \describe{
 #'   \item{YFT_SRD_1A_4}{Simulated aggregated 1 area datasets for YFT; Simulation #4}
 #'   \item{biol_dat}{Biological data for YFT}
@@ -52,27 +53,29 @@ NULL
 #' @name YFT_SRD_1A_4
 #' @docType data
 #' @details 
-#' each sim list with following:
+#' Each simulation specific list object contains the following:
 #' @usage data('YFT_SRD_1A_4')
 #' @examples 
 #' data('YFT_SRD_1A_4')
 #' head(biol_dat)
 NULL
 
-#' YFT_4area_observations_1-100
+#' 4 AREA SINGLE REPRESENTATIVE DATASET
 #'
-#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#' A single representative datasets aggregated at the 4 area scale. The chosen dataset is simulation #4.
+#' List objects include aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+
 #'
-#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' @format A data frame 'YFT_SRD_1A_4.RData' with variables:
 #' \describe{
-#'   \item{dat_4A_4}{Simulated aggregated 4 area datasets for YFT; X=1:100}
+#'   \item{dat_4A_4}{Simulated aggregated 1 area datasets for YFT; Simulation #4}
 #'   \item{biol_dat}{Biological data for YFT}
 #' }
 #' 
 #' @name YFT_SRD_4A_4
 #' @docType data
 #' @details 
-#' each sim list with following:
+#' Each simulation specific list object contains the following:
 #' @usage data('YFT_SRD_4A_4')
 #' @examples 
 #' data('YFT_SRD_4A_4')
