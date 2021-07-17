846aa1329dcdd2901074dc8ba5b81ce22ace1655
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-07-16 23:26:10 -0700

    pkgdown site edits

--- R/data.R
+++ R/data.R
@@ -1,44 +1,3 @@
-#' YFT_1area_observations_1-100
-#'
-#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
-#'
-#' @format A data frame 'YFT_1area_observations_1-100.RData' with variables:
-#' \describe{
-#'   \item{dat_1A_X}{Simulated aggregated 1 area datasets for YFT; X=1:100}
-#'   \item{biol_dat}{Biological data for YFT}
-#' }
-#' 
-#' @name YFT_1area_observations_1-100
-#' @docType data
-#' @details 
-#' each sim list with following:
-#' @usage data('YFT_1area_observations_1-100')
-#' @examples 
-#' data('YFT_1area_observations_1-100')
-#' head(biol_dat)
-#' 
-NULL
-
-#' YFT_4area_observations_1-100
-#'
-#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
-#'
-#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
-#' \describe{
-#'   \item{dat_4A_X}{Simulated aggregated 4 area datasets for YFT; X=1:100}
-#'   \item{biol_dat}{Biological data for YFT}
-#' }
-#' 
-#' @name YFT_4area_observations_1-100
-#' @docType data
-#' @details 
-#' each sim list with following:
-#' @usage data('YFT_4area_observations_1-100')
-#' @examples 
-#' data('YFT_4area_observations_1-100')
-#' head(biol_dat)
-NULL
-
 #' 1 area single representative dataset
 #'
 #' A single representative datasets aggregated at the 1 area scale. The chosen dataset is simulation #4.
@@ -79,6 +38,47 @@ NULL
 #' @usage data('YFT_SRD_4A_4')
 #' @examples 
 #' data('YFT_SRD_4A_4')
+#' head(biol_dat)
+NULL
+
+#' 100 simulated YFT datasets at the 1 area scale
+#'
+#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#'
+#' @format A data frame 'YFT_1area_observations_1-100.RData' with variables:
+#' \describe{
+#'   \item{dat_1A_X}{Simulated aggregated 1 area datasets for YFT; X=1:100}
+#'   \item{biol_dat}{Biological data for YFT}
+#' }
+#' 
+#' @name YFT_1area_observations_1-100
+#' @docType data
+#' @details 
+#' each sim list with following:
+#' @usage data('YFT_1area_observations_1-100')
+#' @examples 
+#' data('YFT_1area_observations_1-100')
+#' head(biol_dat)
+#' 
+NULL
+
+#' 100 simulated YFT datasets at the 4 area scale
+#'
+#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#'
+#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' \describe{
+#'   \item{dat_4A_X}{Simulated aggregated 4 area datasets for YFT; X=1:100}
+#'   \item{biol_dat}{Biological data for YFT}
+#' }
+#' 
+#' @name YFT_4area_observations_1-100
+#' @docType data
+#' @details 
+#' each sim list with following:
+#' @usage data('YFT_4area_observations_1-100')
+#' @examples 
+#' data('YFT_4area_observations_1-100')
 #' head(biol_dat)
 NULL
 
