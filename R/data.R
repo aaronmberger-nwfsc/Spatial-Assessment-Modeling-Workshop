dec5bca323235f89a641a448b736472319270c6e
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-07-17 01:04:56 -0700

    updates to pkgdown

--- R/data.R
+++ R/data.R
@@ -5,14 +5,33 @@
 #'
 #' @format A data frame 'YFT_SRD_1A_4.RData' with variables:
 #' \describe{
-#'   \item{YFT_SRD_1A_4}{Simulated aggregated 1 area datasets for YFT; Simulation #4}
+#'   \item{dat_4A_44}{Simulated aggregated 1 area datasets for YFT; Simulation #4}
 #'   \item{biol_dat}{Biological data for YFT}
 #' }
 #' 
 #' @name YFT_SRD_1A_4
 #' @docType data
 #' @details 
-#' Each simulation specific list object contains the following:
+#' The key lists in YFT_SRD_1A_$ (e.g., dat_1A_1$lencomp) include: 
+#' 
+#' lencomp: (list) 1 area dataframe of aggregated length frequencies by length bin with columns corresponding to pseudo-year (Yr), season(Seas), fleet/survey index (FltSvy), gender (Gender),  partition – length data come from all fish encountered (Part=0), number of lengths sampled (Nsamp), and the set of length bins (purse seine only)
+#' catch: (list) 1 area dataframe of catch by fishery, including pseudo-year (year), and    season (seas)
+#' CPUE/cpu: (list) 1 area cpue with columns for pseudo-year (year), season (seas), cpue (cpu), standard deviation associated with lognormal error in cpue (cv), and survey index (index); note that CPUE is for longline only
+#' tag_releases: 1 area tag release data with columns corresponding to generic tag release event number (tg), area of release (reg), pseudo-year (yr), season (season), gender (gender), age (age), and number of releases (nrel) adjusted for initial tag mortality 
+#' tag_recaps: 1 area tag recapture data with columns corresponding to generic tag release event number (tg), pseudo-year (yr), season (season), fleet index (fleet), and the number of recaptures (recaps)
+#' 
+#' The lists in biol_dat include:
+#' 
+#' M: age varying natural mortality
+#' age: first age to last age (pseudo-years)
+#' maturity: maturity at age ogive (pseudo-years)
+#' k: age varying growth coefficients
+#' L: length (cm) 
+#' Linf: length infinity (cm)
+#' Lmin: length minimum (cm)
+#' a: length-weight scaling coefficient
+#' b: length-weight shape parameter
+
 #' @usage data('YFT_SRD_1A_4')
 #' @examples 
 #' data('YFT_SRD_1A_4')
@@ -45,13 +64,13 @@ NULL
 #'
 #' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
 #'
-#' @format A data frame 'YFT_1area_observations_1-100.RData' with variables:
+#' @format A data frame 'YFT_1area_observations_1_100.RData' with variables:
 #' \describe{
 #'   \item{dat_1A_X}{Simulated aggregated 1 area datasets for YFT; X=1:100}
 #'   \item{biol_dat}{Biological data for YFT}
 #' }
 #' 
-#' @name YFT_1area_observations_1-100
+#' @name YFT_1area_observations_1_100
 #' @docType data
 #' @details 
 #' each sim list with following:
@@ -66,13 +85,13 @@ NULL
 #'
 #' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
 #'
-#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' @format A data frame 'YFT_4area_observations_1_100.RData' with variables:
 #' \describe{
 #'   \item{dat_4A_X}{Simulated aggregated 4 area datasets for YFT; X=1:100}
 #'   \item{biol_dat}{Biological data for YFT}
 #' }
 #' 
-#' @name YFT_4area_observations_1-100
+#' @name YFT_4area_observations_1_100
 #' @docType data
 #' @details 
 #' each sim list with following:
