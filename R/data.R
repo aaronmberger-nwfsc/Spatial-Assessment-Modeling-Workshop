b145cba5532494b24cb8005b67572bac0cf809b2
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-07-16 22:48:36 -0700

    pkgdown edits

--- R/data.R
+++ R/data.R
@@ -38,3 +38,44 @@ NULL
 #' data('YFT_4area_observations_1-100')
 #' head(biol_dat)
 NULL
+
+#' YFT_4area_observations_1-100
+#'
+#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#'
+#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' \describe{
+#'   \item{YFT_SRD_1A_4}{Simulated aggregated 1 area datasets for YFT; Simulation #4}
+#'   \item{biol_dat}{Biological data for YFT}
+#' }
+#' 
+#' @name YFT_SRD_1A_4
+#' @docType data
+#' @details 
+#' each sim list with following:
+#' @usage data('YFT_SRD_1A_4')
+#' @examples 
+#' data('YFT_SRD_1A_4')
+#' head(biol_dat)
+NULL
+
+#' YFT_4area_observations_1-100
+#'
+#' A dataset 100 simulations of the YFT 1 area aggregated catch, cpue, length frequencies, and tagging data (along with biological data).
+#'
+#' @format A data frame 'YFT_4area_observations_1-100.RData' with variables:
+#' \describe{
+#'   \item{dat_4A_4}{Simulated aggregated 4 area datasets for YFT; X=1:100}
+#'   \item{biol_dat}{Biological data for YFT}
+#' }
+#' 
+#' @name YFT_SRD_4A_4
+#' @docType data
+#' @details 
+#' each sim list with following:
+#' @usage data('YFT_SRD_4A_4')
+#' @examples 
+#' data('YFT_SRD_4A_4')
+#' head(biol_dat)
+NULL
+
