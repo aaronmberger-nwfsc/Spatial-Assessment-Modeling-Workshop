6da492cf98b53f0964da4be5f8aa84c1b5c5ecf3
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-11-09 22:56:07 -0800

    pkgdown fixes

---
+++ vignettes/OM_description_YFT.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  options(rmarkdown.html_vignette.check_title = FALSE)
+)
+
+## ----setup--------------------------------------------------------------------
+library(SpatialAssessmentModelingWorkshop)
+
---
+++ vignettes/TOA_OM_Description.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  options(rmarkdown.html_vignette.check_title = FALSE)
+)
+
+## ----echo=FALSE, out.width='75%',fig.align="center"---------------------------
+knitr::opts_chunk$set(file.path="Fig1_TOA.png")
+
