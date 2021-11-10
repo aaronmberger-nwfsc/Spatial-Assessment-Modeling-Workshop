8af476b3a1bdf4bfc9de54d1d1dca29b86141e8a
carenbarcelo-NOAA <62513493+carenbarcelo-NOAA@users.noreply.github.com>
2021-11-09 23:31:05 -0800

    swap .rmd for TOA OM description for attempted fix

---
+++ vignettes/OM_Description_TOA.R
@@ -0,0 +1,13 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  options(rmarkdown.html_vignette.check_title = FALSE)
+)
+
+## ----setup--------------------------------------------------------------------
+library(SpatialAssessmentModelingWorkshop)
+
+## ----echo=FALSE, out.width='75%',fig.align="center"---------------------------
+knitr::opts_chunk$set(file.path="Fig1_TOA.png")
+
